b94e69b64bdc16ec80005ebe268e6a3f44c0f5d9
medham86 <eng.medhathamada@gmail.com>
2023-09-29 14:08:00 +0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
   const config = {
+    base: "/quran",
     plugins: [react()],
-    base: '/',
   }
 
   if (command !== 'serve') {
